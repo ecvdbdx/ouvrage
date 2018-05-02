9e0910543ad468477d68f4ef2d78442f675fe392
Romain Crestey <rcrestey@gmail.com>
2018-05-02 23:46:48 +0200

    basic configuration

--- src/Root.test.jsx
+++ src/Root.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Root from './Root';
 
+
 it('renders correctly', () => {
   const tree = renderer
     .create(<Root />)
