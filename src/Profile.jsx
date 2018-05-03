ba8244ab1584fba907117e1775f0bb2491ec4af5
maxencedewaele <maxencedewaele@live.fr>
2018-05-03 17:50:32 +0200

    Clean component profile + tests

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
-
 export default class Profile extends React.Component {
   render () {
     return (
       <div>
         <h1>Profile</h1>
-        <img src={this.props.student.avatar} alt="avatar"/>
-        <p>{this.props.student.first_name},  {this.props.student.last_name}</p>
-        <p>{this.props.student.email}</p>
-        <p>{this.props.student.birthdate}</p>
+        <div className="avatar">
+          <img src={this.props.student.avatar} alt="avatar"/>
+        </div>
+        <div className="content">
+          <p>{this.props.student.first_name},  {this.props.student.last_name}</p>
+          <p>{this.props.student.email}</p>
+          <p>{this.props.student.birthdate}</p>
+        </div>
       </div>
     );
   }
--- src/Root.jsx
+++ src/Root.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import Profile from './Profile.jsx';
 
-const ProfileLink = props => (<a onClick={props.handleClick} style={{ cursor: 'pointer' }}>click me!</a>);
-
 export default class Root extends React.Component {
   constructor (props) {
     super(props);
 
     this.state = {
-      student: this.props.students[0]
-    };
-
-    this.loadProfile = this.loadProfile.bind(this);
-  }
-  loadProfile () {
-    this.setState({
       student: this.props.students[1]
-    });
+    };
   }
 
 
@@ -24,7 +15,6 @@ export default class Root extends React.Component {
     return (
       <div>
         <h1>Ouvrage</h1>
-        <ProfileLink handleClick={this.loadProfile}/>
         <Profile student={this.state.student}/>
       </div>
     );
