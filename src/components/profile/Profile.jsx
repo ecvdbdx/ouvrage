ef48f0243a2be0aecd7cfda4a3264faa31d3bf34
maxencedewaele <maxencedewaele@live.fr>
2018-05-24 18:21:54 +0200

    Add display of repo list github api

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -39,9 +39,8 @@ export default class Profile extends React.Component {
       .then(resp => {
         if(resp.ok) {
           resp.json().then(res => {
-            console.log(res[0]);
             this.setState({
-              githubRepoData: res
+              githubRepoData: res.slice(0, 3)
             });
           });
         } else {
@@ -74,6 +73,14 @@ export default class Profile extends React.Component {
 
   render () {
     const { id, firstName, lastName, username, email, avatar } = this.props.student;
+    const repos = this.state.githubRepoData.map((repo) =>
+      <li key={repo.id}>
+        <img className="repo-icon" src="https://camo.githubusercontent.com/7710b43d0476b6f6d4b4b2865e35c108f69991f3/68747470733a2f2f7777772e69636f6e66696e6465722e636f6d2f646174612f69636f6e732f6f637469636f6e732f313032342f6d61726b2d6769746875622d3235362e706e67"/>
+        <a href={repo.html_url} target="blank">
+          {repo.name}
+        </a>
+      </li>
+    );
 
     return (
       <div id="show-profile">
@@ -129,7 +136,8 @@ export default class Profile extends React.Component {
                 <p><span className="key">Following : </span>{this.state.githubData.following}</p>
                 <p><span className="key">Public repos : </span>{this.state.githubData.public_repos}</p>
                 <ul>
-                  {/*<li>{this.state.githubRepoData.0.name}</li>*/}
+                  {repos}
+                  {this.state.githubData.public_repos > 3 ? <a target="_blank" href={this.state.githubData.html_url}>En voir plus</a> : ''}
                 </ul>
               </div>
             </div>
